eeaa02263ee79b4412b9e121311f0a9c49b0ee28
ashley <ashley.oketo@student.moringaschool.com>
2022-12-05 00:31:45 +0300

    Initial files

---
+++ src/components/Navbar.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav
+        className="navbar navbar-expand-lg navbar-light bg-white py-3
+      shadow-sm"
+      >
+        <div className="container">
+          <NavLink className="navbar-brand fw-bold fs-4" href="#">
+            QUOTES APP
+          </NavLink>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+              <div className="buttons">
+                <NavLink to="/login" className="btn btn-outline-dark me-4">
+                  Login
+                </NavLink>
+              </div>
+              <li className="nav-item">
+                <NavLink className="nav-link active" aria-current="page" to="/">
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/quotes">
+                  Quotes
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
