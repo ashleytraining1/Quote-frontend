3d428436a536f85c13f8e8d890806f2fda02324d
ashley <ashley.oketo@student.moringaschool.com>
2022-12-07 10:01:59 +0300

    Installed react router dom and applied its features for routing in app.js and index.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import { Routes, Route } from "react-router-dom";
+import Quotes from "./components/Quote";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/quotes" element={<Quotes />  }/>
+       
+      </Routes>
       <Footer />
     </>
   );
